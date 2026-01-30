dfac47ce117e6e3235bda905cccdbbe582397fb6
Yesid542 <yesidbarreto24@gmail.com>
2026-01-30 10:26:05 -0500

    analitics vercel

--- src/components/Principal/principal.jsx
+++ src/components/Principal/principal.jsx
@@ -5,6 +5,7 @@ import './principal.css'
 import LandingHeader from '../Header/header'
 import LandingFooter from '../Footer/footer'
 import { Link } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/next';
 
 function Principal (){
 
@@ -322,6 +323,7 @@ function Principal (){
             </div>
           </div>
         </section>  
+        <Analytics />
 
         <LandingFooter/>
         </>
